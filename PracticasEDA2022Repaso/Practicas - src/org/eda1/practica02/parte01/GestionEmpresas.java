c9773fb76cde332b02717fe34bed6f1c7c56c956
HUNOR MORICZI <hm586@inlumine.ual.es>
2022-01-30 21:53:53 +0100

    Practica 2 parte 1 3 tests Verde // Trabajando con AVLTree()

--- PracticasEDA2022Repaso/Practicas - src/org/eda1/practica02/parte01/GestionEmpresas.java
+++ PracticasEDA2022Repaso/Practicas - src/org/eda1/practica02/parte01/GestionEmpresas.java
@@ -28,6 +28,9 @@ public class GestionEmpresas {
 			//Leer archivo de entrada
 			//Si la linea empieza por @ o está vacía --> la ignoramos (usar continue)
 			//...
+			if (line.isEmpty()) continue;
+			if (line.startsWith("@")) continue;
+			items = line.split(" - ");		
 			add(items[0], items[1], items[2]);
 		}
 		scan.close();
@@ -37,7 +40,13 @@ public class GestionEmpresas {
 		//Seguimos con la misma lógica: objeto auxiliar para buscar en la estructura
 		//Objeto current que puede ser null o almacenar la referencia al objeto en caso de que exista
 		Empresa empresaAux = new Empresa(empresaID);
-		Empresa empresaCurr = //...
+		Empresa empresaCurr = this.empresas.find(empresaAux);
+		if (empresaCurr == null ) {
+			empresas.add(empresaAux);
+		}else {
+			empresaAux = empresaCurr;
+		}
+		empresaAux.add(proyectoID, ciudad);
 		//...
 		return empresaCurr == null;
 	}
@@ -47,14 +56,19 @@ public class GestionEmpresas {
 	}
 
 	public int numeroProyectosEmpresa(String empresaID) {
-		Empresa empresaCurr = //...
-		return //... 1 linea...hacemos uso del "if compacto"
+		Empresa empresaCurr = this.empresas.find(new Empresa(empresaID));
+		return (empresaCurr == null) ? -1 : empresaCurr.size();
 	}
 
 	public int numeroCiudadesProyecto(String proyectoID) {
 		Proyecto proyectoCurr = null;
 		//1 for()
 		//...
+		for (Empresa empresa : this.empresas) {
+			proyectoCurr = empresa.findProyecto(new Proyecto(proyectoID));
+			if (proyectoCurr == null) continue;
+			return proyectoCurr.size();
+		}
 		return -1;
 	}
 
@@ -63,6 +77,16 @@ public class GestionEmpresas {
 		//Tendremos que comprobar que la empresa existe
 		//2 for() anidados
 		//...
+		Empresa empresaCurr = this.empresas.find(new Empresa(empresaID));
+		
+		if (empresaCurr == null) return -1;
+		ciudades = new ArrayList<String>();
+		for (Proyecto proyecto : empresaCurr) {
+			for (String ciudad : proyecto) {
+				if (ciudades.contains(ciudad)) continue;
+				ciudades.add(ciudad);
+			}
+		}
 		return ciudades.size();
 	}
 
@@ -79,6 +103,14 @@ public class GestionEmpresas {
 		ArrayList<String> empresasIDaux = new ArrayList<String>();
 		//2 for() anidados
 		//...
+		for (Empresa empresa : empresas) {
+			for (Proyecto proyecto : empresa) {
+				if (proyecto.contains(ciudad)) {
+					empresasIDaux.add(empresa.getEmpresaID());
+					break;
+				}
+			}
+		}
 		return empresasIDaux.toString();
 	}
 
@@ -86,6 +118,13 @@ public class GestionEmpresas {
 		ArrayList<String> proyectosIDaux = new ArrayList<String>();
 		//2 for() anidados
 		//...
+		for (Empresa empresa : empresas) {
+			for (Proyecto proyecto : empresa) {
+				if (proyecto.contains(ciudad)) {
+					proyectosIDaux.add(proyecto.getProyectoID());
+				}
+			}
+		}
 		return proyectosIDaux.toString();
 	}
 	
@@ -94,6 +133,15 @@ public class GestionEmpresas {
 		 //Tendremos que comprobar que la empresa existe
 		 //2 for() anidados
 		 //...
+		 Empresa empresa = empresas.find(new Empresa(empresaID));
+		 if (empresa == null) return "[]";
+		 ciudades = new ArrayList<String>();
+		 for (Proyecto proyecto : empresa) {
+			for (String ciudad : proyecto) {
+				if (ciudades.contains(ciudad)) continue;
+				ciudades.add(ciudad);
+			}
+		}
 		 return ciudades.toString();
 	}
 }
